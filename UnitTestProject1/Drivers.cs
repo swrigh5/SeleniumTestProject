536b84a6b52fd8ef3961fa81882aad12eab7f028
swrigh5 <swright2133@gmail.com>
2019-01-23 23:21:22 -0500

    Add project files.

---
+++ UnitTestProject1/Drivers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace UnitTestProject1
+{
+    public enum BrowserType
+    {
+        Chrome,
+        FireFox,
+        Edge
+    }
+
+    class WebDriverFactory
+    {
+        public IWebDriver Create(BrowserType browserType)
+        {
+            switch (browserType)
+            {
+                case BrowserType.Chrome:
+                    return GetChromeDriver();
+                case BrowserType.FireFox:
+                    throw new NotImplementedException();
+                case BrowserType.Edge:
+                    throw new NotImplementedException();
+                default:
+                    throw new ArgumentOutOfRangeException("No browser exists");
+                
+            }
+               
+        }
+
+
+        private IWebDriver GetChromeDriver()
+        {
+            var outputDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new ChromeDriver(outputDirectory);
+        }
+
+        public IWebDriver GetFireFoxDriver()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IWebDriver GetEdgeDriver()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+   
+    [TestFixture]
+    public class UnitTest1
+    {
+        public IWebDriver driver { get; private set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new WebDriverFactory().Create(BrowserType.Chrome);
+            driver.Navigate().GoToUrl("https://www.ultimateqa.com/simple-html-elements-for-automation/");
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
+ 
