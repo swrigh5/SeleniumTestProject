8bf0e565bdb478bda78ec2072300213db48bf30a
Stephen Wright <Stephen.Wright@amtrustgroup.com>
2019-01-28 17:04:04 -0500

    add manipulating elements file

---
+++ UnitTestProject1/ManipulatingElements.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    class ManipulatingElements
+    {
+        public IWebDriver driver { get; private set; }
+
+        [SetUp]
+        public void setup()
+        {
+            driver = new WebDriverFactory().Create(BrowserType.Chrome);
+            driver.Navigate().GoToUrl("https://www.ultimateqa.com/filling-out-forms/");
+        }
+
+
+        [Test]
+        public void test1()
+        {
+            ///clear name field
+            ///type into field
+            ///
+            ///clear text field
+            ///type into field
+            ///submit
+            
+        }
+
+    }
+}
