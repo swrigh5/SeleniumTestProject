9362f89f8b69a8568e0e053bdd9b853774c74f8c
swrigh5 <swright2133@gmail.com>
2019-02-02 17:11:41 -0500

    removed helperclass file, message2captcha test

--- UnitTestProject1/Quizzes/ElementManipulationQuiz.cs
+++ UnitTestProject1/Quizzes/ElementManipulationQuiz.cs
@@ -28,27 +28,41 @@ namespace UnitTestProject1.Quizzes
             driver = new WebDriverFactory().Create(BrowserType.Chrome);
             driver.Navigate().GoToUrl("https://www.ultimateqa.com/filling-out-forms");
 
-            var contact = driver.FindElement(By.Id("ed_pb_contact_name_1"));
+            var contact = driver.FindElement(By.Id("et_pb_contact_name_1"));
             contact.Clear();
             contact.SendKeys("Jeffrey");
 
-            var msg = driver.FindElement(By.Id("et_bp_contact_message_1"));
+            var msg = driver.FindElement(By.Id("et_pb_contact_message_1"));
             msg.Clear();
             msg.SendKeys("Hello there");
 
             var captchaTxt = driver.FindElement(By.ClassName("et_pb_contact_captcha_question")).Text;
+            var answer = AddStrings(captchaTxt);
 
-            var regex = Regex.Matches(captchaTxt, @"[1-9][0-9]?\s{1}");
-            
+            driver
+                .FindElement(By.XPath("//*[@class='input et_pb_contact_captcha']"))
+                .SendKeys(answer);
 
+
+            driver
+                .FindElements(By.XPath("//*[@class='et_pb_contact_submit et_pb_button']"))[1]
+                .Click();      
         }
 
 
-        [Test]
-        private void ConvertStringtoInteger()
+        private string AddStrings(string captchaText)
         {
+            var regex = Regex.Matches(captchaText, @"[1-9][0-9]?");
+            int sum = 0;
 
+            foreach (Match value in regex)
+            {
+                sum += int.Parse(value.Value);
+            }
+
+            return sum.ToString();
         }
+
 
 
     }
