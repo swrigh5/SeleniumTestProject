0052ea85bcc4b30a05a2ead4c9dfd29a61f74a45
Stephen Wright <Stephen.Wright@amtrustgroup.com>
2019-03-14 14:05:03 -0400

    drag and drop

---
+++ UnitTestProject1/MouseAndKeyboardActions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    class MouseAndKeyboardActions
+    {
+        private IWebDriver driver { get; set; }
+
+        [SetUp]
+        public void setup()
+        {
+            driver = new WebDriverFactory().Create(BrowserType.Chrome);
+            driver.Navigate().GoToUrl("https://jqueryui.com/droppable/");
+        }
+
+        [Test]
+        public void DragAndDropTest1()
+        {
+            var actions = new Actions(driver);
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+            wait.Until(ExpectedConditions.FrameToBeAvailableAndSwitchToIt(By.ClassName("demo-frame")));
+
+            var sourceElement = driver.FindElement(By.Id("draggable"));
+            var targetElement = driver.FindElement(By.Id("droppable"));
+
+            actions.DragAndDrop(sourceElement, targetElement).Perform();
+
+
+            Assert.That(targetElement.Text, Is.EqualTo("Dropped!"));
+        }
+
+        [Test]
+        public void DragAndDropTest2()
+        {
+            var actions = new Actions(driver);
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+            wait.Until(ExpectedConditions.FrameToBeAvailableAndSwitchToIt(By.ClassName("demo-frame")));
+
+
+            var sourceElement = driver.FindElement(By.Id("draggable"));
+            var targetElement = driver.FindElement(By.Id("droppable"));
+
+            var drag = actions
+                 .ClickAndHold(sourceElement)
+                 .MoveToElement(targetElement)
+                 .Release(targetElement)
+                 .Build();
+
+            drag.Perform();
+
+            Assert.That(targetElement.Text, Is.EqualTo("Dropped!"));
+
+        }
+    }
+}
---
+++ UnitTestProject1/Quizzes/DragAndDropQuiz.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+
+namespace UnitTestProject1.Quizzes
+{
+    class DragAndDropQuiz
+    {
+        private IWebDriver driver { get; set; }
+
+        [SetUp]
+        public void setup()
+        {
+            driver = new WebDriverFactory().Create(BrowserType.Chrome);
+            driver.Navigate().GoToUrl("http://www.pureexample.com/jquery-ui/basic-droppable.html");
+        }
+
+        [Test]
+        public void DragAndDropQuiz1()
+        {
+
+            var actions = new Actions(driver);
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.FrameToBeAvailableAndSwitchToIt(By.Id("ExampleFrame-94")));
+
+            //assert "moving out!"
+            var sourceElement = driver.FindElement(By.XPath("//*[@class='square ui-draggable']"));
+            var targetElement = driver.FindElement(By.XPath("//*[@class='squaredotted ui-droppable']"));
+         
+            var drag = actions
+                .ClickAndHold(sourceElement)
+                .MoveToElement(targetElement)
+                .Build();
+
+            drag.Perform();
+
+            var message = driver.FindElement(By.XPath("//*[@id='info']"));
+            Assert.That(message.Text, Is.EqualTo("moving in!"));
+
+            var drop = actions
+                .Release(targetElement)
+                .Build();
+
+            drop.Perform();
+
+            message = driver.FindElement(By.XPath("//*[@id='info']"));
+            Assert.That(message.Text, Is.EqualTo("dropped!"));
+        
+        }
+
+    }
+}
